49cd931c35ab8c13b70d5e1e2fc0a7cd9d7b8111
Chris Zubak-Skees <chriszs@gmail.com>
2016-07-06 12:12:43 -0400

    Try to fix it

--- src/util/fec/check.js
+++ src/util/fec/check.js
@@ -1,6 +1,6 @@
 var async = require('async'),
     request = require('request'),
-    models  = require('../../models'),
+    models = require('../../models'),
     fs = require('fs'),
     filingQueue = require('./import'),
     progress = require('progress-stream');
@@ -11,56 +11,57 @@ var lookAhead = 100,
 
 var temp_dir = path.resolve(__dirname + '/../../data/fec/downloaded');
 
-function checkForFiling(filing_id,cb) {
+function checkForFiling(filing_id, cb) {
     var filePath = temp_dir + '/' + filing_id + '.fec';
 
-    fs.exists(filePath,function (exists) {
+    fs.exists(filePath, function(exists) {
         if (!exists) {
             console.log('checking ' + filing_id);
 
             var str = progress({
-                time: 10000
+                time: 100
             });
 
             var r = request('http://docquery.fec.gov/dcdev/posted/' + filing_id + '.fec');
-            r.on('response', function (resp) {
+            r.on('response', function(resp) {
                 if (resp.statusCode == 200) {
-                    str.setLength(parseInt(resp.headers['content-length']));
+                    var length = parseInt(resp.headers['content-length']);
+                    str.setLength(length);
+
+                    str.on('progress', function(progress) {
+                        console.log(progress.percentage + '% transferred');
+                    });
 
                     r.on('error', function(err) {
-                        console.log(err);
+                            console.log(err);
 
-                        setTimeout(cb,interval);
-                    })
-                    .on('end',function () {
-                        console.log('downloaded ' + filing_id);
+                            setTimeout(cb, interval);
+                        })
+                        .pipe(str)
+                        .pipe(fs.createWriteStream(filePath))
+                        .on('end', function() {
+                            console.log('downloaded ' + filing_id);
 
-                        if (str.progress.transferred !== str.progress.length) {
-                            console.warn('expecting a file of size ' + str.progress.length + ' but downloaded file is ' + str.progress.transferred);
-                        }
-
-                        filingQueue.push({
-                            name: filing_id + '',
-                            openStream: function (cb) {
-                                cb(null,fs.createReadStream(filePath));
+                            if (str.progress.transferred !== length) {
+                                console.warn('expecting a file of size ' + length +
+                                    ' but downloaded file is ' + str.progress.transferred);
                             }
-                        });
 
-                        setTimeout(cb,interval);
-                    })
-                    .on('progress',function (progress) {
-                        console.log(progress.percentage + '% transferred');
-                    })
-                    .pipe(str)
-                    .pipe(fs.createWriteStream(filePath));
-                }
-                else {
+                            filingQueue.push({
+                                name: filing_id + '',
+                                openStream: function(cb) {
+                                    cb(null, fs.createReadStream(filePath));
+                                }
+                            });
+
+                            setTimeout(cb, interval);
+                        });
+                } else {
                     console.log('not found');
-                    setTimeout(cb,interval);
+                    setTimeout(cb, interval);
                 }
             });
-        }
-        else {
+        } else {
             cb();
         }
     });
@@ -68,25 +69,27 @@ function checkForFiling(filing_id,cb) {
 
 function queueFilingsToCheck() {
     models.fec_filing.findAll({
-        attributes: ['filing_id'],
-        limit: lookBehind,
-        order: [['filing_id','DESC']]
-    })
-    .then(function (filings) {
-        filings = filings.map(function (filing) {
-            return filing.filing_id;
-        });
+            attributes: ['filing_id'],
+            limit: lookBehind,
+            order: [
+                ['filing_id', 'DESC']
+            ]
+        })
+        .then(function(filings) {
+            filings = filings.map(function(filing) {
+                return filing.filing_id;
+            });
 
-        var q = async.queue(checkForFiling,1);
+            var q = async.queue(checkForFiling, 1);
 
-        for (var i = filings[filings.length-1]; i <= filings[0]+lookAhead; i++) {
-            if (filings.indexOf(i) === -1) {
-                q.push(i);
+            for (var i = filings[filings.length - 1]; i <= filings[0] + lookAhead; i++) {
+                if (filings.indexOf(i) === -1) {
+                    q.push(i);
+                }
             }
-        }
 
-        q.drain = queueFilingsToCheck;
-    });
+            q.drain = queueFilingsToCheck;
+        });
 }
 
 queueFilingsToCheck();
