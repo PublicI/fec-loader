de85b91dcf7dc4fd6a79e12a3169b4632d583529
Chris Zubak-Skees <chriszs@gmail.com>
2016-12-13 18:03:52 -0500

    Add directory-based filing import strategy

---
+++ src/util/fec/dir.js
@@ -0,0 +1,32 @@
+var fs = require('fs'),
+    filingQueue = require('./import');
+
+var filings_dir = __dirname + '/../../data/fec/downloaded';
+
+function init(filings_dir) {
+    fs.readdir(filings_dir, function(err, files) {
+        if (err) throw err;
+
+        filingQueue.drain = null;
+
+        files.forEach(function (file) {
+            if (file.indexOf('.fec') === -1) {
+                return;
+            }
+
+            filingQueue.push({
+                name: file,
+                openStream: function (cb) {
+                    fs.createReadStream(filings_dir + '/' + file, cb);
+                }
+            });
+        });
+
+        filingQueue.drain = function () {
+            console.log('done');
+        };
+
+    });
+}
+
+init(filings_dir);
