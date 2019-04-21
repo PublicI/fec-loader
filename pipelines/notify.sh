2db17b92c0b5e58e5d162284a417bc509e85d151
Chris Zubak-Skees <chriszs@gmail.com>
2019-04-20 21:05:40 -0400

    Add notification

---
+++ pipelines/notify.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for file in $(find /pfs/filings/ -name "*.fec.gz");
+do
+	SUMMARY=$(gunzip < $file | head -n 10 | fec2json | sed -n 2p)
+	ID=$(echo $file | tr -dc '0-9')
+
+	if echo $SUMMARY | jq -e 'has("committee_name") and has("form_type") and has("filer_committee_id_number") and .form_type != "F24A" and .form_type != "F24N"' > /dev/null; then
+		PAYLOAD=$(echo $SUMMARY | jq --compact-output --arg filing_id $ID '{$filing_id,form_type,committee_name,organization_name,filer_committee_id_number,coverage_from_date,coverage_through_date,col_a_total_receipts,col_a_total_disbursements,col_a_cash_on_hand_close_of_period}')
+		PAYLOAD=${PAYLOAD//\'/\\\'}
+		psql -c "NOTIFY fecImportStart, '"$PAYLOAD"';"
+	fi
+done
