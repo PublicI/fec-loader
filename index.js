e92c5341fe100c420654392a0135e188be4b9a6b
Chris Zubak-Skees <chriszs@gmail.com>
2019-05-12 20:27:20 -0400

    Refactor the CLI to use a series of subcommands with help and update notifications

---
+++ index.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    api: require('./lib/api'),
+    create: require('./lib/create'),
+    increment: require('./lib/increment'),
+    json: require('./lib/json'),
+    psql: require('./lib/psql'),
+    rss: require('./lib/rss')
+};
