344c7dcb7df30b238cade6e22c70931e029748be
Chris Zubak-Skees <chriszs@gmail.com>
2019-06-05 10:32:14 -0400

    Add missing do

--- transforms/download-zips.sh
+++ transforms/download-zips.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 for file in $(find $1 -name "*.json");
+do
 	DIR=$(basename $(dirname $file))
 	BASE=$(basename $file .json)
     aws s3 cp s3://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74/bulk-data/$DIR/$BASE.zip $2$DIR --region us-gov-west-1 --no-sign-request
