2d34de55965099019ec4346c7e10167d7c7c13f4
Chris Zubak-Skees <chriszs@gmail.com>
2019-06-05 10:10:21 -0400

    Download zips

--- transforms/download-zips.sh
+++ transforms/download-zips.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 for file in $(find $1 -name "*.json");
 	DIR=$(basename $(dirname $file))
-	BASE=$(basename $file)
-    aws s3 cp s3://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74/bulk-data/$DIR/$BASE $2 --region us-gov-west-1 --no-sign-request
+	BASE=$(basename $file .json)
+    aws s3 cp s3://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74/bulk-data/$DIR/$BASE.zip $2$DIR --region us-gov-west-1 --no-sign-request
 done
