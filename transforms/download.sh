8a355985a82b78686752b9485d1f70f46f8e6dea
Chris Zubak-Skees <chriszs@gmail.com>
2019-06-20 18:12:15 -0400

    Switch FEC URL to HTTPS

--- transforms/download.sh
+++ transforms/download.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 for file in $(find $1 -name "*.fec");
 do
-	curl -s "http://docquery.fec.gov/dcdev/posted/$(basename $file)" | gzip -9 > $2"$(basename $file).gz"
+	curl -s "https://docquery.fec.gov/dcdev/posted/$(basename $file)" | gzip -9 > $2"$(basename $file).gz"
 done
