d207a6995891d255276a5890194ac5d26f9c3733
Chris Zubak-Skees <chriszs@gmail.com>
2019-07-15 13:06:54 -0400

    Don't load if filing exists

--- transforms/load.sh
+++ transforms/load.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 
 for file in $(find $1 -name "*.fec.gz");
 do
-	gunzip < $file | ./bin/fec convert --format=psql $(echo $file | tr -dc '0-9') | (echo "DELETE FROM fec_filings WHERE filing_id = $(echo $file | tr -dc '0-9');" && cat) | psql -v ON_ERROR_STOP=on --single-transaction 2> $2$(echo $file | tr -dc '0-9')".log"
+	filing=$(psql -qtA -c "SELECT * FROM fec_filings WHERE filing_id = $(echo $file | tr -dc '0-9');")
+	if [ $(echo $filing | wc -l) -eq "0" ]; then
+		gunzip < $file | ./bin/fec convert --format=psql $(echo $file | tr -dc '0-9') | psql -v ON_ERROR_STOP=on --single-transaction 2> $2$(echo $file | tr -dc '0-9')".log"
+	fi
 done
