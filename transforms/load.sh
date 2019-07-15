35c92000639872fc2c957dd947012d9c73c341b1
Chris Zubak-Skees <chriszs@gmail.com>
2019-07-15 14:50:30 -0400

    Output errors in addition to logging them

--- transforms/load.sh
+++ transforms/load.sh
@@ -6,6 +6,7 @@ for file in $(find $1 -name "*.fec.gz");
 do
 	filing=$(psql -qtA -c "SELECT count(*) FROM fec_filings WHERE filing_id = $(echo $file | tr -dc '0-9');")
 	if [ $filing -eq "0" ]; then
-		gunzip < $file | ./bin/fec convert --format=psql $(echo $file | tr -dc '0-9') | psql -v ON_ERROR_STOP=on --single-transaction 2> $2$(echo $file | tr -dc '0-9')".log"
+		logfile=$2$(echo $file | tr -dc '0-9')".log"
+		gunzip < $file | ./bin/fec convert --format=psql $(echo $file | tr -dc '0-9') | psql -v ON_ERROR_STOP=on --single-transaction 2> >(tee $logfile)
 	fi
 done
