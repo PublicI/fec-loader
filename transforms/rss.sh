e664ecf585ea20f7df49f0fa1b74358add5ba803
Chris Zubak-Skees <chriszs@gmail.com>
2019-10-15 01:32:08 -0400

    Overwrite cron output

--- transforms/rss.sh
+++ transforms/rss.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 ./bin/fec list --rss --headers=false --format=tsv --columns=fec_url | head -n 100 | while read url; do
-	echo "$(echo $url | tr -dc '0-9')" > $1"$(echo $url | tr -dc '0-9').fec";
+	touch $1"$(echo $url | tr -dc '0-9').fec";
 done
