d89da9c0ae30834c29f40eaae31d30b7a96a5281
Chris Zubak-Skees <chriszs@gmail.com>
2019-10-15 00:41:09 -0400

    Try holding the file's timestamp constant

--- transforms/rss.sh
+++ transforms/rss.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 ./bin/fec list --rss --headers=false --format=tsv --columns=fec_url | head -n 100 | while read url; do
-	touch $1"$(echo $url | tr -dc '0-9').fec";
+	touch -t 201910150000 $1"$(echo $url | tr -dc '0-9').fec";
 done
