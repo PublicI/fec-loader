3323d1f91d52f2ee5d87b64b0e8e5e641fdc054b
Chris Zubak-Skees <chriszs@gmail.com>
2019-10-15 16:25:16 -0400

    Add test fixtures for future regression tests

--- tests/lib/ndjson.test.js
+++ tests/lib/ndjson.test.js
@@ -4,7 +4,7 @@ const ndjson = require('../../lib/ndjson');
 const fs = require('fs');
 
 test('ndjson', async done => {
-    const input = fs.createReadStream(__dirname + '/../fixtures/989514.fec');
+    const input = fs.createReadStream(__dirname + '/../fixtures/romney-f3p.fec');
     const writer = new ObjectWritableMock();
 
     ndjson({
